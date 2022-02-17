eb463d07d2130e9f556da31ec69d15fb3119e4e6
GabrielaSaboia <gs525@njit.edu>
2022-02-17 18:49:08 -0500

    Added int converter to the utilities namespace

---
+++ Utilities/CustomIntConverter.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace Utilities;
+
+public class CustomIntConverter : DefaultTypeConverter
+{
+    public override object ConvertFromString(String text, IReaderRow row, MemberMapData mamberMapData)
+    {
+        if (text != "")
+        {
+            return int.Parse(text);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
